3bf556e3c094f9fe287166973dd17278b9c21d79
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-12-04 17:12:18 -0600

    merge

--- navigation/index.js
+++ navigation/index.js
@@ -11,7 +11,7 @@ import OnboardingScreen from "../screens/OnboardingScreen";
 import UserForm from "../screens/UserForm";
 import BottomTabNavigator from "./BottomTabNavigator";
 import LinkingConfiguration from "./LinkingConfiguration";
-import UserForm from "../components/UserForm";
+import UserForm from "../screens/UserForm";
 import Leaderboard from "../components/Leaderboard";
 import Showering from "../components/Showering";
 
